698d0eeaa1f4769a3b3b51744c11b4ba6ee4245f
Carmen-Git-It <carmenwhitton@gmail.com>
2022-04-28 16:08:27 -0400

    Mounted bodyParser middleware

--- myApp.js
+++ myApp.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+
 console.log("Hello World");
 
 app.use((req, res, next) => {
     console.log(req.method + " " + req.path + " - " + req.ip);
     next();
 });
+
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.use("/public", express.static(__dirname + "/public"));
 
