0653d078205ad931473491c154b99faebb71b77c
Carmen-Git-It <carmenwhitton@gmail.com>
2022-04-27 20:39:35 -0400

    Added static resources to express route

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 
-app.use("/", __dirname + "/public");
+app.use("/", express.static(__dirname + "/public"));
 
 
 
